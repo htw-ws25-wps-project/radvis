d9636e93e2b5fb7cb059595ba4d1291430928a8c
Sophia Lehmann <sophia.lehmann@student.htw-berlin.de>
2026-01-17 00:45:22 +0100

    feat(report): map SaveReportCommand to Report using builder

---
+++ backend/src/main/java/de/wps/radvis/backend/mangel/schnittstelle/SaveReportCommandConverter.java
@@ -0,0 +1,33 @@
+package de.wps.radvis.backend.mangel.schnittstelle;
+
+import de.wps.radvis.backend.mangel.domain.entity.Report;
+import lombok.AllArgsConstructor;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
+
+import java.math.BigDecimal;
+
+@AllArgsConstructor
+public class SaveReportCommandConverter {
+    private static final GeometryFactory GEOMETRY_FACTORY = new GeometryFactory();
+
+    public Report toReport(SaveReportCommand command) {
+        Point geometrie = toPoint(command.getLongitude(), command.getLatitude());
+
+        return Report.builder()
+                .issue(command.getIssue())
+                .description(command.getDescription())
+                .geometrie(geometrie)
+                .build();
+    }
+
+    public static Point toPoint(BigDecimal latitude, BigDecimal longitude) {
+        return GEOMETRY_FACTORY.createPoint(
+                new Coordinate(
+                        longitude.doubleValue(),
+                        latitude.doubleValue()
+                )
+        );
+    }
+}
