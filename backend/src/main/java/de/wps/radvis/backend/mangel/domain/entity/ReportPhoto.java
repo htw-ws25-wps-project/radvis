79ed61a08ce9e405a018d666b0f6c7595d8736f0
Gabriela Goncalvez <gabriela.goncalvez@posteo.net>
2026-01-22 18:18:19 +0100

    Implement photo upload and retrieval for reports

--- backend/src/main/java/de/wps/radvis/backend/mangel/domain/entity/ReportPhoto.java
+++ backend/src/main/java/de/wps/radvis/backend/mangel/domain/entity/ReportPhoto.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright (c) 2023 WPS - Workplace Solutions GmbH
+ *
+ * Licensed under the EUPL, Version 1.2 or as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
 package de.wps.radvis.backend.mangel.domain.entity;
 
 import de.wps.radvis.backend.common.domain.entity.AbstractEntity;
@@ -17,43 +30,42 @@ import static org.valid4j.Assertive.require;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ReportPhoto extends AbstractEntity {
 
-    @Lob
-    private byte[] data;
+	@Lob
+	private byte[] data;
 
-    private String filename;
+	private String filename;
 
-    private String contentType;
+	private String contentType;
 
-    private Long size;
+	private Long size;
 
-    @ManyToOne
-    private Report report;
+	@ManyToOne
+	private Report report;
 
-    @Builder
-    private ReportPhoto(Long id, byte[] data, String filename, String contentType, Long size) {
-        super(id);
+	@Builder
+	private ReportPhoto(Long id, byte[] data, String filename, String contentType, Long size) {
+		super(id);
 
-        require(data, notNullValue());
-        require(data.length > 0, "Bild darf nicht leer sein");
-        require(contentType, notNullValue());
+		require(data, notNullValue());
+		require(data.length > 0, "Bild darf nicht leer sein");
+		require(contentType, notNullValue());
 
-        require(isSupportedContentType(contentType),
-                "Ungültiger Dateityp: " + contentType + ". Nur JPG und PNG sind erlaubt.");
+		require(isSupportedContentType(contentType),
+			"Ungültiger Dateityp: " + contentType + ". Nur JPG und PNG sind erlaubt.");
 
+		this.data = data;
+		this.filename = filename;
+		this.contentType = contentType;
+		this.size = size;
+	}
 
-        this.data = data;
-        this.filename = filename;
-        this.contentType = contentType;
-        this.size = size;
-    }
+	private boolean isSupportedContentType(String contentType) {
+		return "image/jpeg".equals(contentType) ||
+			"image/png".equals(contentType);
+	}
 
-    private boolean isSupportedContentType(String contentType) {
-        return "image/jpeg".equals(contentType) ||
-                "image/png".equals(contentType);
-    }
-
-    public void assignToReport(Report report) {
-        require(report, notNullValue());
-        this.report = report;
-    }
+	public void assignToReport(Report report) {
+		require(report, notNullValue());
+		this.report = report;
+	}
 }
--- backend/src/main/java/de/wps/radvis/backend/mangel/schnittstelle/ReportController.java
+++ backend/src/main/java/de/wps/radvis/backend/mangel/schnittstelle/ReportController.java
@@ -1,11 +1,28 @@
+/*
+ * Copyright (c) 2023 WPS - Workplace Solutions GmbH
+ *
+ * Licensed under the EUPL, Version 1.2 or as soon they will be approved by the European Commission - subsequent versions of the EUPL (the "Licence");
+ *
+ * You may not use this work except in compliance with the Licence.
+ * You may obtain a copy of the Licence at:
+ *
+ * https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an "AS IS" basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the Licence for the specific language governing permissions and limitations under the Licence.
+ */
 package de.wps.radvis.backend.mangel.schnittstelle;
 
 import de.wps.radvis.backend.mangel.domain.ReportService;
 import de.wps.radvis.backend.mangel.domain.entity.Report;
+import de.wps.radvis.backend.mangel.domain.entity.ReportPhoto;
+import de.wps.radvis.backend.mangel.schnittstelle.view.ReportPhotoView;
 import de.wps.radvis.backend.mangel.schnittstelle.view.ReportView;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,26 +33,43 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ReportController {
 
-    private final ReportService reportService;
+	private final ReportService reportService;
 
-    @PostMapping(consumes = {"multipart/form-data"})
-    public ResponseEntity<ReportView> createReport(@ModelAttribute @Valid SaveReportCommand command) {
-        Report created = reportService.create(command);
-        return ResponseEntity.status(HttpStatus.CREATED).body(new ReportView(created));
-    }
-    @GetMapping
-    public ResponseEntity<List<ReportView>> getAllReports() {
-        List<ReportView> reports = reportService.getAllReports();
-        return ResponseEntity.ok(reports);
-    }
+	@PostMapping(consumes = { "multipart/form-data" })
+	public ResponseEntity<ReportView> createReport(@ModelAttribute @Valid SaveReportCommand command) {
+		Report created = reportService.create(command);
+		return ResponseEntity.status(HttpStatus.CREATED).body(new ReportView(created));
+	}
 
-    @GetMapping("/{id}")
-    public ResponseEntity<ReportView> getReport(@PathVariable Long id) {
-        try {
-            return ResponseEntity.ok(reportService.getReport(id));
-        } catch (java.util.NoSuchElementException e) {
-            return ResponseEntity.notFound().build();
-        }
-    }
+	@GetMapping
+	public ResponseEntity<List<ReportView>> getAllReports() {
+		List<ReportView> reports = reportService.getAllReports();
+		return ResponseEntity.ok(reports);
+	}
 
+	@GetMapping("/{id}")
+	public ResponseEntity<ReportView> getReport(@PathVariable Long id) {
+		try {
+			return ResponseEntity.ok(reportService.getReport(id));
+		} catch (java.util.NoSuchElementException e) {
+			return ResponseEntity.notFound().build();
+		}
+	}
+
+    @GetMapping("/{reportId}/photos/{photoId}")
+    public ResponseEntity<byte[]> getPhoto(
+            @PathVariable Long reportId,
+            @PathVariable Long photoId) {
+
+        ReportPhotoView photo = reportService.getPhoto(reportId, photoId);
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.parseMediaType(photo.contentType()))
+                .header(
+                        HttpHeaders.CONTENT_DISPOSITION,
+                        "inline; filename=\"" + photo.filename() + "\""
+                )
+                .body(photo.data());
+    }
 }
+
